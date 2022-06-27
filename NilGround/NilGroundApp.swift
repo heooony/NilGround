bb69d745dbfb078cb122a708f4f44fe8a9063532
oueya1479 <oueya1479@naver.com>
2022-06-27 19:36:11 +0900

    Initial Commit

---
+++ NilGround/NilGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NilGroundApp.swift
+//  NilGround
+//
+//  Created by 김동헌 on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct NilGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

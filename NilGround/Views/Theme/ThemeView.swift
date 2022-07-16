b4336849f2f56f44729f4812bbbcbce341d436f3
oueya1479 <oueya1479@naver.com>
2022-07-16 23:42:01 +0900

    add offset

--- NilGround/Views/Theme/ThemeView.swift
+++ NilGround/Views/Theme/ThemeView.swift
@@ -12,6 +12,7 @@ struct ThemeView: View {
     
 //    let themeCellVM: ThemeCellViewModel
     @Environment(\.presentationMode) var presentationMode
+    @State var titleOffset = CGFloat.zero
     let width = UIScreen.main.bounds.width
     @Binding var root: Bool
     
@@ -26,16 +27,16 @@ struct ThemeView: View {
                         Image("profile-1")
                             .resizable()
                             .aspectRatio(contentMode: .fill)
-                            .frame(width: size.width, height: size.height)
-                            .cornerRadius(15)
+                            .frame(width: size.width, height: UIScreen.main.bounds.height)
                         
                         LinearGradient(colors: [
-                            .black.opacity(0.4),
+                            .black.opacity(0.7),
+                            .black.opacity(0.6),
                             .black.opacity(0.3),
-                            .black.opacity(0.1),
                             .clear,
-                            .black.opacity(0.2),
-                            .black.opacity(0.3),
+                            .black.opacity(0.4),
+                            .black.opacity(0.5),
+                            .black.opacity(0.7),
                         ], startPoint: .top, endPoint: .bottom)
                         .cornerRadius(15)
                         
@@ -68,26 +69,47 @@ struct ThemeView: View {
                         .shadow(radius: 5)
                         .padding(.top, getSafeAreaTop())
                         .padding()
+                        
+                        VStack(alignment: .leading, spacing: 10) {
+                            Spacer()
+                            
+                            Text("\(titleOffset)")
+                            
+                            Text("성남의 태양 아래 피톤치드를 느낄 수 있는 12곳")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.primary)
+                            
+                            Divider()
+                                .padding(.horizontal, 20)
+                                .foregroundColor(.secondary)
+                            
+                            Text("\(Date.now.formatted())")
+                                .font(.caption)
+                                .foregroundColor(.secondary)
+                            
+                            Text("성남에는 유명한 탄천이 있습니다. 탄천에서 즐길 수 있는 식물 정원과 바람이 잘 드는 곳으로 선정해보았습니다. 오리역 탄천부터 복정 탄천에 이르기 전 범위로 정해보았습니다.")
+                                .font(.callout)
+                                .foregroundColor(.primary)
+                        }
+                        .padding(.horizontal, 30)
+                        .padding(.bottom, 40)
                     }
                 }
-                .frame(height: 350)
+                // MARK: 여기서부터 수정하기
+                .offset(y: -titleOffset)
+                .frame(height: UIScreen.main.bounds.height)
+                .background(
+                    GeometryReader { proxy in
+                        Color.clear
+                            .preference(key: TitleOffsetKey.self, value: proxy.frame(in: .global).minY
+                            )
+                    }
+                )
+                .onPreferenceChange(TitleOffsetKey.self) { value in
+                    titleOffset = value
+                }
                 
-                Text("성남의 태양 아래 피톤치드를 느낄 수 있는 12곳")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.primary)
-                    .padding(.horizontal, 10)
-                
-                Divider()
-                    .padding(.horizontal, 20)
-                    .foregroundColor(.secondary)
-                    
-                Text("성남에는 유명한 탄천이 있습니다. 탄천에서 즐길 수 있는 식물 정원과 바람이 잘 드는 곳으로 선정해보았습니다. 오리역 탄천부터 복정 탄천에 이르기 전 범위로 정해보았습니다.")
-                    .font(.callout)
-                    .foregroundColor(.primary)
-                    .padding(.horizontal)
-                
-                Color.clear.frame(height: 50)
                 
                 ForEach(1..<7, id: \.self) { index in
                     VStack(alignment: .leading) {
@@ -137,6 +159,13 @@ struct ThemeView: View {
             .first?.windows
             .filter({$0.isKeyWindow}).first
         return (keyWindow?.safeAreaInsets.top)!
+    }
+}
+
+struct TitleOffsetKey: PreferenceKey {
+    static var defaultValue = CGFloat.zero
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value += nextValue()
     }
 }
 
